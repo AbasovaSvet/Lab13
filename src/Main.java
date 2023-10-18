22f064e622aee07bd67b1e14368c1a4d455c12d8
Svetlana <A.svet.mr@yandex.ru>
2023-10-18 14:30:30 +0300

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Как вас зовут?");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        char[] result = name.toCharArray();
+        char x = result[result.length-1];
+        if (x == 'а' ||   x == 'я' ||  x == 'и')
+        {System.out.println("Приветик!");}
+        else {System.out.println("Здарова!");}
     }
 }
